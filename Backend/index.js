dc8fbfd1db10d8ef7b2509fa00a9ca5fab5b3868
utkarshsingh004 <singhutkarshkumar097@gmail.com>
2025-02-27 13:18:03 +0530

    initial commit

--- Backend/index.js
+++ Backend/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI || "mongodb://localhost/RealEstateDatabase", {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
